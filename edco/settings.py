89a76227ce9d10849552e38f1814574c16a3c330
Juan David García Manrique <jdgarcia1381@gmail.com>
2025-10-02 20:00:01 -0500

    Update debug setting. Add CSRF_TRUSTED_ORIGINS setting.

--- edco/settings.py
+++ edco/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
-
+CSRF_TRUSTED_ORIGINS = [os.environ.get("ALLOWED_HOSTS")]
 
 # Application definition
 
